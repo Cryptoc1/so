4b10f282cc7275c323895a346592b4c2f1a8830c
Samuel Steele <steelesam72@gmail.com>
2015-06-03 21:40:15 -0400

    Refactored, and commented code for pull request to @probablyjosh/so

--- so.py
+++ so.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python
+
 # So, a plain English CLI for your favourite movies and shows
 import requests, sys
-
 
 DEVMODE = True
 _usage = "Usage:\n\t./so.py <movie title>"
 
 def main():
+    # Make sure the user supplied some arguments
     if len(sys.argv) > 1:
         do_request(sys.argv)
     else:
         print _usage
 
 def do_request(args):
+    # Make our request
     request = "http://www.omdbapi.com/?t={}".format('%20'.join(args[1:]))
     r = requests.get(request)
-    # Make sure the request didn't fail
+    # Did the request fail?
     if r.status_code == 200:
+        # Nope, it didn't
         resp = r.json()
+        # Wait, we actually got stuff, RIGHT?!?!
         if resp['Response'] != "True":
             if DEVMODE:
                 print 'Response not okay\n'
 	    print r.json()['Error']
+        # Phew, response returned some data
         else:
             if DEVMODE:
                 print 'Response Okay\n'
             for i in resp:
                 print format_resp(resp, i)
     else:
+        # fuck, it did! :,(
         print 'Error: Request was not made'
 
+# Make the data pertty
 def format_resp(list, index):
     return index + ": " + list[index]
 
+# Enter the Matrix...
 if __name__ == "__main__":
     main()
